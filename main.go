60e58b93c20eed004364480ea7aec2ffee7d1654
trapvincenzo <trapvincenzo@gmail.com>
2017-01-11 21:26:30 +0000

    Fix pointer bug

--- main.go
+++ main.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"encoding/json"
 	"io/ioutil"
 	"log"
 	"net/http"
@@ -28,8 +27,25 @@ type ServerConfig struct {
 	Port string `yaml:"port"`
 }
 
+var router *mux.Router
+
+func createRouteHandler(route Route) {
+	router.HandleFunc(route.Path, func(w http.ResponseWriter, r *http.Request) {
+		switch route.ResponseType {
+		case "json":
+			w.Header().Set("Content-Type", "application/json")
+			w.Write([]byte(route.Response))
+		default:
+			w.Header().Set("Content-type", "text/plain")
+			w.Write([]byte(route.Response))
+		}
+
+	}).Methods(route.Methods...)
+}
+
 func main() {
 	content, _ := ioutil.ReadFile("config.yaml")
+	router = mux.NewRouter()
 
 	routes := Routes{}
 	serverConfig := ServerConfig{}
@@ -37,17 +53,8 @@ func main() {
 	yaml.Unmarshal(content, &routes)
 	yaml.Unmarshal(content, &serverConfig)
 
-	router := mux.NewRouter()
 	for _, route := range routes.Routes {
-		router.HandleFunc(route.Path, func(w http.ResponseWriter, r *http.Request) {
-			switch route.ResponseType {
-			case "json":
-				json.NewEncoder(w).Encode(route.Response)
-			default:
-				w.Write([]byte(route.Response))
-			}
-
-		}).Methods(route.Methods...)
+		createRouteHandler(route)
 	}
 
 	log.Fatal(http.ListenAndServe(serverConfig.Port, router))
